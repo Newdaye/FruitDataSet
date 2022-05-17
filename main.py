1dee296521c37910be9296a9f0e3500a05da0008
Riley Weasel Fat <riley.weaselfat@uleth.ca>
2022-05-17 13:44:58 -0600

    Data is available

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import numpy as np
 
 
 def getData(file):
-    pass
+    data = pd.read_excel(file)
+    return data
 
 
 def main():
-    pass
+    data = getData("Date_Fruit_Datasets/Date_Fruit_Datasets.xlsx")
+    print(data.shape)
+
 
 if __name__ == "__main__":
     main()
